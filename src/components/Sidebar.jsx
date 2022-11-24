4de50199ee099896b74a1c762c3867ad45bdfab9
Witty Wayne <debjyotinandi76@gmail.com>
2022-11-25 01:28:13 +0530

    added music player functionality

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+const Sidebar = () => (
+  <div>Sidebar</div>
+);
+
+export default Sidebar;
+
+//sidebar 1:05:46
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '9ca5830ab5msh4af566c358f39a9p1c1b41jsn26dc10484d8e',
+//     'X-RapidAPI-Host': ''
+//   }
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+  export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) =>{
+            headers.set('X-RapidAPI-Key', '9ca5830ab5msh4af566c358f39a9p1c1b41jsn26dc10484d8e')
+            
+
+            return headers; 
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({query: ()=> '/charts/world'}),
+    }),
+  });
+
+  export const {
+    useGetTopChartsQuery,
+  } = shazamCoreApi;
