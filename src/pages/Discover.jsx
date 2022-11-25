d37abc63c6450c00d44882d2dbd5e79ac8577505
Witty Wayne <debjyotinandi76@gmail.com>
2022-11-25 19:02:04 +0530

    sidebar implemented

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -21,7 +21,7 @@ const Discover = () =>{
 
     <div className="flex flex-col">
         <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-            <h2 className='font-bold text-3xl text-white text-left'>Discover</h2>
+            <h2 className='font-bold text-3xl text-white text-left'>{`Discover ${genreTitle}`}</h2>
         <select
             onChange={()=>{}}
             value = ""
